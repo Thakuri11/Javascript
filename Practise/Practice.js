e51f06433f4d1d5396c87bed9ad1c023c25c795b
Sumanta106 <thakurisumanta@gmail.com>
2025-12-21 14:54:31 +0545

    Add initial JavaScript files and HTML structure for basic functionality

---
+++ Practise/Practice.js
@@ -0,0 +1,39 @@
+
+//Q.finding the max value in array
+let arr = [1, 2, 3, 100, 4];
+let max = arr[0];
+arr.forEach((el) => {
+  if (el > max) {
+    max = el;
+  }
+});
+console.log(max);
+
+//Q.Reverse the number
+let n =5011;
+let result=0;
+while(n>0){
+let b = n % 10;
+result=result*10 +b;
+
+ n = Math.floor(n / 10);
+}
+console.log(result);
+
+//Q. USe of objects
+const obj={
+    name:"Rahul",
+    age:"21"
+};
+document.getElementById("try").innerHTML="My name is " + obj.name +" and I am " +obj.age +" years old";
+
+//change text when clicked!
+function ClickMe() {
+  document.getElementById("add").innerHTML = "Text changed!";
+}
+
+//using addEventListener[perform without innnerHtml]
+const abc=document.getElementById("next");
+abc.addEventListener("click",function(){
+    alert("Button Clicked!");
+})
